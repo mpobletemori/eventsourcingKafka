024611226c0a71dfaaf02c4239e13c033e4a8252
mpobletemori <mpobletemori@gmail.com>
2022-03-28 07:02:31 -0500

    Agregar producers y events

--- banking-account/account.cmd/src/main/java/com/banking/account/cmd/infraestructure/AccountEventProducer.java
+++ banking-account/account.cmd/src/main/java/com/banking/account/cmd/infraestructure/AccountEventProducer.java
@@ -4,7 +4,9 @@ import com.banking.cqrs.core.events.BaseEvent;
 import com.banking.cqrs.core.producers.EventProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AccountEventProducer implements EventProducer {
 
     @Autowired
--- banking-account/account.cmd/src/main/java/com/banking/account/cmd/infraestructure/AccountEventStore.java
+++ banking-account/account.cmd/src/main/java/com/banking/account/cmd/infraestructure/AccountEventStore.java
@@ -7,9 +7,11 @@ import com.banking.cqrs.core.events.EventModel;
 import com.banking.cqrs.core.exceptions.AggregateNotFoundException;
 import com.banking.cqrs.core.exceptions.ConcurrencyExcepction;
 import com.banking.cqrs.core.infrastructure.EventStore;
+import com.banking.cqrs.core.producers.EventProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.Date;
 import java.util.List;
@@ -21,6 +23,9 @@ public class AccountEventStore implements EventStore {
 
     @Autowired
     private EventStoreRepository eventStoreRepository;
+
+    @Autowired
+    private EventProducer eventProducer;
 
     @Override
     public void saveEvents(String aggregateId, Iterable<BaseEvent> events, int expectedVersion) {
@@ -44,10 +49,9 @@ public class AccountEventStore implements EventStore {
                     .build();
 
             var persistedEvent=this.eventStoreRepository.save(eventModel);
-            if(Objects.nonNull(persistedEvent)){
-                //producir event en kafka
+            if(!StringUtils.hasLength(persistedEvent.getId())){
+                this.eventProducer.producer(event.getClass().getSimpleName(),event);
             }
-
         }
     }
 
